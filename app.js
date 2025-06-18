bf53417d254db8f88d54665ad4d862e551984409
ncrim7 <100636010+ncrim7@users.noreply.github.com>
2025-06-18 09:39:16 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -22,9 +22,9 @@ hbs.registerPartials(partialsPath)
 
 app.use(express.static(publicPath))
 
-
-app.listen(3000,  ()=>{
-    console.log('server is up on port 3000')
+const port = process.env.PORT || 3000
+app.listen(port,  ()=>{
+    console.log(`server is up on port ${port}`)
 })
 
 app.get('', (req,res) => {
